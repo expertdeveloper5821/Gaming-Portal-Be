2448564d2353e30b6819323382d2c31366aa7a7c
developerAkshay1 <akshay.kumar@technogetic.com>
2023-07-14 16:47:11 +0530

    db connection added

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,1 +1,16 @@
-// databse connection here
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+
+dotenv.config();
+
+const url = process.env.DB_URL || ''
+
+mongoose.connect(
+    url )
+   .then(() => {
+     console.log("Database Connected 👍️");
+   })
+   .catch((error) => {
+     console.log("Database not connected", error);
+   });
+ 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from "express";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import './config/db'
 const cors = require('cors')
 
 const app: Application = express();
@@ -10,17 +10,10 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Connect to MongoDB
-mongoose
-  .connect("mongodb://localhost:27017/mydatabase", {
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("MongoDB connection error:", error));
-
 // Routes
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, world!");
-});
+})
 
 // Start the server
 app.listen(port, () => {
