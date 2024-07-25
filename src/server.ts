75e1b3bf8c836d3aa9bee820dd3c41f37ba45d1d
expertdeveloper5821 <expert.developer.5821@gmail.com>
2024-07-25 12:06:28 +0530

    some changes on server js file

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,7 @@ import { environmentConfig } from "./config/environmentConfig";
 import { printSuccess, printError } from "./utils/consoleMessage";
 import { setupSocketIO } from "./middlewares/socket";
 
-const port: number =
-  (process.env.PORT as unknown as number) || environmentConfig.SERVER_PORT;
+const port: number = Number(process.env.PORT) || environmentConfig.SERVER_PORT;
 
 console.log(port, "port");
 console.log(
@@ -19,16 +18,16 @@ console.log(environmentConfig.ENV, environmentConfig.SERVER_PORT);
 
 const server = http.createServer(app);
 
-// Set up Socket.io
+// Set up Socket.IO
 export const io = setupSocketIO(server);
 app.locals.io = io;
 
-// sample get route
+// Sample GET route
 app.get("/", (req, res) => {
   res.status(200).send("Hello, Gamers!");
 });
 
-// server listening
+// Server listening
 server.listen(port, () => {
   printSuccess(`Server is running on port ${port}...👍️`);
 
@@ -38,3 +37,5 @@ server.listen(port, () => {
     printError(`Server could not start on port ${port}...😵`);
   }
 });
+
+export default server; // Ensure the server is exported
